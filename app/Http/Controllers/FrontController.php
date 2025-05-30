4d8667b8acc8b6b6d6ea593a104fc760b15d521c
SuryaDarma4k <suryadarma4k@gmail.com>
2025-05-30 15:15:51 +0700

    Menampilkan produk dari brand

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -18,5 +19,16 @@ class FrontController extends Controller
     public function category(Category $category){
         session()->put('category_id', $category->id);
         return view('front.brands', compact('category'));
+    }
+
+    public function brand(Brand $brand){
+        $category_id = session()->get('category_id');
+
+        $products = Product::where('brand_id', $brand->id)
+        ->where('category_id', $category_id)
+        ->latest()
+        ->get();
+
+        return view('front.gadgets', compact('brand', 'products'));
     }
 }
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -14,7 +14,7 @@
 <body>
     <main class="max-w-[640px] mx-auto min-h-screen flex flex-col relative has-[#Bottom-nav]:pb-[144px]">
         <div id="Top-navbar" class="flex items-center justify-between pt-5 px-5">
-            <a href="index.html" class="flex shrink-0">
+            <a href="{{ route('front.index') }}" class="flex shrink-0">
                 <img src="{{ asset('assets/images/logos/logo.svg') }}" alt="logo" />
             </a>
             <a href="#" class="w-11 h-11 flex shrink-0">
@@ -50,7 +50,7 @@
             <div class="swiper w-full h-fit">
                 <div class="swiper-wrapper">
                     @forelse ($latest_products as $item_latest_product)
-                        <a href="details.html" class="swiper-slide max-w-[150px] first-of-type:ml-5 last-of-type:mr-5">
+                        <a href="{{ route('front.details', $item_latest_product->slug) }}" class="swiper-slide max-w-[150px] first-of-type:ml-5 last-of-type:mr-5">
                             <div class="flex flex-col gap-3 bg-white">
                                 <div
                                     class="h-[130px] flex shrink-0 items-center rounded-2xl overflow-hidden bg-[#F6F6F6]">
@@ -89,7 +89,7 @@
             <h2 class="font-semibold text-lg leading-[27px]">You Might Like</h2>
             <div class="flex flex-col gap-5">
                 @forelse ($random_products as $random_product)
-                    <a href="details.html" class="card">
+                    <a href="{{ route('front.details', $random_product->slug) }}" class="card">
                         <div class="flex items-center gap-3">
                             <div class="w-20 h-20 flex shrink-0 rounded-2xl overflow-hidden bg-[#F6F6F6] items-center">
                                 <div class="w-full h-[50px] flex shrink-0 justify-center">
@@ -124,7 +124,7 @@
             <div class="bg-white/50 backdrop-blur-sm absolute w-full h-full"></div>
             <ul class="flex items-center gap-3 justify-evenly p-5 relative z-10">
                 <li>
-                    <a href="index.html">
+                    <a href="{{ route('front.index') }}">
                         <div
                             class="group flex flex-col items-center text-center gap-2 transition-all duration-300 hover:text-black">
                             <div class="w-6 h-6 flex shrink-0">
